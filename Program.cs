87ff64e2f540ce273b946c0806ca758a40372056
Cihan DİNÇER <201701010@kocaeli.edu.tr>
2021-02-07 19:07:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> ogrenciler = new MyList<string>();
+
+
+            ogrenciler.Add("Mert");
+            ogrenciler.Add("Murat");
+            ogrenciler.Add("Yunus");
+
+            Console.WriteLine("Öğrenci sayısı: " + ogrenciler.Length);
+
+            
+            
+            foreach (var ogrenci in ogrenciler.Items)
+            {
+                Console.WriteLine("Öğrenciler: " + ogrenci);
+                Console.ReadLine();
+                Console.WriteLine("===============================================");
+            }
+
+            
+        }
+    }
+
+
+    class MyList<C>
+    {
+        C[] items;
+
+        public MyList()
+        {
+            items = new C[0];
+        }
+
+        public void Add(C item)
+        {
+            C[] tempArray = items;
+            items = new C[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return items.Length;}
+        }
+
+        public C[] Items
+        {
+            get { return items; }
+        }
+    }
+}
